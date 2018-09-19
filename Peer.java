f0acc310dfec01c155637a3fe7b38e5fe863da58
Lars Strodthoff - MacBook Pro <lars.strodthoff@web.de>
2018-09-19 15:13:16 +0200

    Add shared key on active peer side

--- Peer.java
+++ Peer.java
@@ -407,9 +407,17 @@ public class Peer {
                     System.exit(1);
                 }
 
+                if (theirKey <= 0) {
+                    throw new IllegalArgumentException("Expected their key to be > 0");
+                }
+
+                long sharedKey = expmod(theirKey, x, n);
+
+
                 System.out.println("Recap:");
                 System.out.println("My key: " + exchangeKey);
                 System.out.println("Their key: " + theirKey);
+                System.out.println("Our shared key: " + sharedKey);
 
             } else {
                 System.out.println("The proposal was not acknowledged.");
