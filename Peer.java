91579595a7d037221b6199a7d89f13ad2bf85727
Lars Strodthoff - MacBook Pro <lars.strodthoff@web.de>
2018-09-19 12:28:27 +0200

    Implement initial active peer side

--- Peer.java
+++ Peer.java
@@ -14,250 +14,254 @@ import java.util.StringTokenizer;
  * protocol.
  *
  * @author Christian Grimme, 2018
- *
  */
 public class Peer {
 
-	private static Socket socket = null;
-	private static BufferedReader inputStream;
-	private static PrintWriter outputStream;
+    private static Socket socket = null;
+    private static BufferedReader inputStream;
+    private static PrintWriter outputStream;
 
-	/**
-	 * Method use to set up the reader and writer streams for an active socket.
-	 * Used internally by waitForConnect() and connect().
-	 */
-	private static void setup() {
-		try {
-			inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    /**
+     * Method use to set up the reader and writer streams for an active socket.
+     * Used internally by waitForConnect() and connect().
+     */
+    private static void setup() {
+        try {
+            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			outputStream = new PrintWriter(socket.getOutputStream(), true);
+            outputStream = new PrintWriter(socket.getOutputStream(), true);
 
-		} catch (IOException e) {
-			System.out.println("Reader or Writer could not be initialized.");
-			System.exit(1);
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("Reader or Writer could not be initialized.");
+            System.exit(1);
+        }
+    }
 
-	/**
-	 * Method which waits for a connection request from a peer. This method
-	 * should be used in passive mode, to keep on waiting. Therefore a
-	 * SocketServer is set up to listen on the given port for a specified period
-	 * of time.
-	 *
-	 * @param port
-	 *            Post to listen on for connection requests.
-	 * @param timeout
-	 *            Maximum waiting time (milliseconds)
-	 * @return Boolean value (true/false) indicating, whether a socket was
-	 *         created.
-	 */
-	private static boolean waitForConnect(int port, int timeout) {
-		if (socket == null) {
-			ServerSocket ssocket;
-			try {
+    /**
+     * Method which waits for a connection request from a peer. This method
+     * should be used in passive mode, to keep on waiting. Therefore a
+     * SocketServer is set up to listen on the given port for a specified period
+     * of time.
+     *
+     * @param port    Post to listen on for connection requests.
+     * @param timeout Maximum waiting time (milliseconds)
+     * @return Boolean value (true/false) indicating, whether a socket was
+     * created.
+     */
+    private static boolean waitForConnect(int port, int timeout) {
+        if (socket == null) {
+            ServerSocket ssocket;
+            try {
 
-				ssocket = new ServerSocket();
+                ssocket = new ServerSocket();
 
-				System.out.println("Starting up.");
+                System.out.println("Starting up.");
 
-				ssocket.bind(new InetSocketAddress(port));
+                ssocket.bind(new InetSocketAddress(port));
 
-				System.out.println("Waiting at port " + port);
+                System.out.println("Waiting at port " + port);
 
-				try {
+                try {
 
-					ssocket.setSoTimeout(timeout);
-					socket = ssocket.accept();
+                    ssocket.setSoTimeout(timeout);
+                    socket = ssocket.accept();
 
-					System.out.println("Socket connection accepted.");
+                    System.out.println("Socket connection accepted.");
 
-					setup();
+                    setup();
 
-				} catch (SocketTimeoutException ste) {
-					System.out.println("A Timeout occured. No connection possible.");
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
-				} catch (SecurityException se) {
-					se.printStackTrace();
-				} catch (IllegalArgumentException iae) {
-					iae.printStackTrace();
-				} finally {
-					ssocket.close();
-				}
+                } catch (SocketTimeoutException ste) {
+                    System.out.println("A Timeout occured. No connection possible.");
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } catch (SecurityException se) {
+                    se.printStackTrace();
+                } catch (IllegalArgumentException iae) {
+                    iae.printStackTrace();
+                } finally {
+                    ssocket.close();
+                }
 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return socket != null;
-		} else {
-			System.out.println("Socket already exists. Only one connection allowed.");
-			return true;
-		}
-	}
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return socket != null;
+        } else {
+            System.out.println("Socket already exists. Only one connection allowed.");
+            return true;
+        }
+    }
 
-	/**
-	 * Method to start a connection. This method complements the
-	 * <code>waitForConnect</code> method. It should be used to actively
-	 * initialize a socket connection.
-	 *
-	 * @param ip
-	 *            Contact IP address.
-	 * @param port
-	 *            Contact port.
-	 * @return Boolean (true/false) that indicates whether a connection has been
-	 *         established.
-	 */
-	private static boolean connect(String ip, int port) {
-		if (socket == null) {
-			socket = new Socket();
+    /**
+     * Method to start a connection. This method complements the
+     * <code>waitForConnect</code> method. It should be used to actively
+     * initialize a socket connection.
+     *
+     * @param ip   Contact IP address.
+     * @param port Contact port.
+     * @return Boolean (true/false) that indicates whether a connection has been
+     * established.
+     */
+    private static boolean connect(String ip, int port) {
+        if (socket == null) {
+            socket = new Socket();
 
-			try {
-				socket.connect(new InetSocketAddress(ip, port));
-				System.out.println("Socket connection successful established.");
+            try {
+                socket.connect(new InetSocketAddress(ip, port));
+                System.out.println("Socket connection successful established.");
 
-				setup();
+                setup();
 
-			} catch (IOException e) {
-				System.out.println("Connection to peer impossible: " + e.getLocalizedMessage());
-				socket = null;
-			}
+            } catch (IOException e) {
+                System.out.println("Connection to peer impossible: " + e.getLocalizedMessage());
+                socket = null;
+            }
 
-			return socket != null;
-		} else {
-			System.out.println("Socket already exists. Only one connection allowed.");
-			return true;
-		}
-	}
+            return socket != null;
+        } else {
+            System.out.println("Socket already exists. Only one connection allowed.");
+            return true;
+        }
+    }
 
-	/**
-	 * Method for sending a message string via the socket connection.
-	 *
-	 * @param msg
-	 *            The message sent.
-	 */
-	private static void send(String msg) {
-		outputStream.println(msg);
-	}
+    /**
+     * Method for sending a message string via the socket connection.
+     *
+     * @param msg The message sent.
+     */
+    private static void send(String msg) {
+        outputStream.println(msg);
+    }
 
-	/**
-	 * Method to wait (blocking) for a message via the socket connection.
-	 *
-	 * @return The message received.
-	 * @throws IOException
-	 */
-	private static String waitFor() throws IOException {
-		String currData = null;
-		while (currData == null) {
-			currData = inputStream.readLine();
-		}
-		return currData;
-	}
+    /**
+     * Method to wait (blocking) for a message via the socket connection.
+     *
+     * @return The message received.
+     * @throws IOException
+     */
+    private static String waitFor() throws IOException {
+        String currData = null;
+        while (currData == null) {
+            currData = inputStream.readLine();
+        }
+        return currData;
+    }
 
-	/**
-	 * Utility method for tokenizing a given string into a String array
-	 * respecting a given delimiter.
-	 *
-	 * @param s
-	 *            The String that shall be tokenized.
-	 * @param delim
-	 *            The delimiter separating the tokens.
-	 * @return Array of tokens
-	 */
-	private static String[] tokenize(String s, String delim) {
-		StringTokenizer st = new StringTokenizer(s, delim);
-		String[] tokens = new String[st.countTokens()];
-		int i = 0;
-		while (st.hasMoreTokens()) {
-			tokens[i] = st.nextToken();
-			i++;
-		}
-		return tokens;
-	}
+    /**
+     * Utility method for tokenizing a given string into a String array
+     * respecting a given delimiter.
+     *
+     * @param s     The String that shall be tokenized.
+     * @param delim The delimiter separating the tokens.
+     * @return Array of tokens
+     */
+    private static String[] tokenize(String s, String delim) {
+        StringTokenizer st = new StringTokenizer(s, delim);
+        String[] tokens = new String[st.countTokens()];
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            tokens[i] = st.nextToken();
+            i++;
+        }
+        return tokens;
+    }
 
-	private static long expmod(int a, int exp, int mod) {
+    private static long expmod(int a, int exp, int mod) {
 
-		/*
-		 * Implement efficient modular exponentiation (see java.math.BigInteger)
-		 */
+        /*
+         * Implement efficient modular exponentiation (see java.math.BigInteger)
+         */
 
-		return 0;
-	}
+        return 0;
+    }
 
-	/**
-	 * Executes the key exchange protocol in passive mode (i.e., waiting for an
-	 * active peer to contact this one)
-	 *
-	 * @param port
-	 *            Port to listen for incoming messages.
-	 */
-	private static void passiveMode(int port) {
+    /**
+     * Executes the key exchange protocol in passive mode (i.e., waiting for an
+     * active peer to contact this one)
+     *
+     * @param port Port to listen for incoming messages.
+     */
+    private static void passiveMode(int port) {
 
-		waitForConnect(port, 200000);
+        waitForConnect(port, 200000);
 
-		int a = 0;
-		int n = 0;
+        int a = 0;
+        int n = 0;
 
-		/*
-		 * Wait for a PROP (propose) command comprising n and a values
-		 */
+        /*
+         * Wait for a PROP (propose) command comprising n and a values
+         */
 
-		/*
-		 * Exchange key
-		 */
+        /*
+         * Exchange key
+         */
 
-	}
+    }
 
-	/**
-	 * Executes the peer in active mode (i.e., this peer starts the DH key
-	 * exchange with a PROP message, sending a and n.
-	 *
-	 * @param ip
-	 *            The remote IP.
-	 * @param port
-	 *            The port to send to.
-	 */
-	private static void activeMode(String ip, int port) {
+    /**
+     * Executes the peer in active mode (i.e., this peer starts the DH key
+     * exchange with a PROP message, sending a and n.
+     *
+     * @param ip   The remote IP.
+     * @param port The port to send to.
+     */
+    private static void activeMode(String ip, int port) {
 
-		int a = 53;
-		int n = 123457;
+        int a = 53;
+        int n = 123457;
+        /* TODO: implement PRNG for a and n */
 
-		connect(ip, port);
+        connect(ip, port);
 
-		/*
-		 * Send PROP (propose) command comprising n and a values
-		 */
+        /*
+         * Send PROP (propose) command comprising n and a values
+         */
 
-		/*
-		 * Exchange key
-		 */
+        send("PROP " + a + " " + n);
 
-	}
+        /*
+         * Exchange key
+         */
 
-	// ----------------------- MAIN METHOD ---------------------
-	public static void main(String[] args) {
+        try {
+            String answer = waitFor();
 
-		if (args.length == 1 && args[0].equals("passive")) {
+            if (answer.equals("ACK")) {
+                System.out.println("The proposal of a and n was acknowledged.");
+            } else {
+                System.out.println("The proposal was not acknowledged.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-			passiveMode(1234);
+    }
 
-		} else {
+    // ----------------------- MAIN METHOD ---------------------
+    public static void main(String[] args) {
 
-			if (args.length == 1 && args[0].equals("active")) {
+        if (args.length == 1 && args[0].equals("passive")) {
 
-				activeMode("127.0.0.1", 1234);
+            passiveMode(1234);
 
-			} else {
-				System.out.println("Usage: java Peer.java <active|passive>");
-			}
-		}
+        } else {
 
-		try {
-			socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            if (args.length == 1 && args[0].equals("active")) {
+
+                activeMode("10.67.24.120", 1234);
+
+            } else {
+                System.out.println("Usage: java Peer.java <active|passive>");
+            }
+        }
+
+        try {
+            socket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
